88afe76de0cf8e83c796f7ed1c38f92521d4ce18
Shuhei-pp <lotakotayota@gmail.com>
2022-03-26 00:01:36 +0900

    textファイルに文字追加できるようにしたで！

--- git_auto.sh
+++ git_auto.sh
@@ -4,8 +4,10 @@
 /bin/echo -n "コミットメッセージ:" 
 read COMMIT_MESSAGE
 
+# commit_only.txtに文字追加
 /bin/echo -n "追加内容:" 
 read ADD_TEXT
+echo $ADD_TEXT >> commit_only.txt
 
 eval "git add ."
 eval "git commit -m '$COMMIT_MESSAGE'"
